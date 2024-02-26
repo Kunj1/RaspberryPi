790f05fc3936be327a753918d971773c2e357d76
Kunj1 <69568292+Kunj1@users.noreply.github.com>
2024-02-26 15:22:18 +0530

    Update and rename h1.py to facedetection.py

--- facedetection.py
+++ facedetection.py
@@ -4,11 +4,10 @@ from deepface import DeepFace
 os.chdir(r'D:/Kunj/raspberry pi/images/0')
 file_names=os.listdir()
 for file in file_names:
-    start=time.now()
     if (file.endswith(".jpg")):
         print(file)
-        result=DeepFace.verify(img1_path=file,img2_path=r"D:/Kunj/raspberry pi/images/1/edited_img.jpg",enforce_detection=False)
-        if (result['verified']):
+        result=DeepFace.verify(img1_path=file,img2_path=r"D:/Kunj/raspberry pi/images/1/2.jpg",model_name='Facenet', detector_backend='mtcnn', enforce_detection=False)
+        if (result['distance']<0.15):
             print("Door Open")
         else:
             print("F off")
